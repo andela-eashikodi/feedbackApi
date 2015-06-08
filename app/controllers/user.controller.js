4ede6aa2388c700504451715087581d530e99efa
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-06-08 17:46:19 +0100

    Add jwt

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -3,15 +3,41 @@
 require('../models/user.model');
 
 var mongoose = require('mongoose');
+var config = require('../../config/config');
+var User = mongoose.model('User'); 
+var jwt = require('jsonwebtoken');
 
-var User = mongoose.model('User');
-
-exports.getUsers = function(req, res){
-  User.find(function(err, users){
-    if(err){
-      return res.json(err);
+exports.auth = function(req, res) {
+  User.findOne({email: req.body.email}, function(err, user){
+    if(err) {
+      res.json(err);
     }
-    return res.json(users);
+
+    if(!user) {
+      res.json({
+        success: false, 
+        message: 'auth failed'});
+    }
+    else if (user) {
+      var validPassword = user.comparePassword(req.body.password);
+      if (!validPassword) {
+        res.json ({
+          success:false,
+          message: 'Auth failed. wrong password'
+        });
+      }
+      else {
+        var token = jwt.sign(user, config[process.env.NODE_ENV]['secret'],{
+          expiresInMinutes:1440
+        });
+
+        res.json({
+          success:true,
+          message: 'token Created',
+          token:token
+        });
+      }
+    }
   });
 };
 
@@ -21,6 +47,15 @@ exports.createUser = function(req, res){
       return res.json(err);
     }
     return res.json(user);
+  });
+};
+
+exports.getUsers = function(req, res){
+  User.find(function(err, users){
+    if(err){
+      return res.json(err);
+    }
+    return res.json(users);
   });
 };
 
