4d4465dd910e95d87017cc3ca323e515191f9147
andela-eashikodi <emeka.ashikodi@andela.co>
2015-06-08 13:30:10 +0100

    Create User schema

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,9 +1,59 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
-var userFeedback = new mongoose.Schema({
-  mail : String
+var userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: "Please enter your firstname"
+  },
+  lastname: {
+    type: String,
+    required: "Please enter your lastname"
+  },
+  email: {
+    type: String,
+    required: "Please enter your email",
+    unique: true
+  },
+  password: {
+    type: String,
+    required: "Please enter your password"
+  },
+  created_at: Date,
+
+  updated_at: Date
+
 });
 
-mongoose.model('User', userFeedback);
+userSchema.pre('save', function(next){
+  var user = this;
+
+  var currentDate = new Date();
+  
+  user.updated_at = currentDate;
+
+  if (!user.created_at){
+    user.created_at = currentDate;
+  }
+
+  if(!user.isModified('password')){
+    return next();
+  }
+
+  bcrypt.hash(user.password, null, null, function(err, hash){
+    if (err){
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
+
+userSchema.methods.comparePassword = function(password){
+  var user = this;
+  return bcrypt.compareSync(password, user.password);
+};
+
+mongoose.model('User', userSchema);
