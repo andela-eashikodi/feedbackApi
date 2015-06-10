8d71ac04a57be9b060e30ea17a30cb38d5fe82ff
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-06-10 22:41:29 +0100

    Add FacebookUser Schema

--- app/models/biz.model.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var mongoose = require('mongoose');
-
-var bizFeedback = new mongoose.Schema({
-  name : String
-});
-
-mongoose.model('Biz', bizFeedback);
--- app/routes/biz.route.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app){
-  var biz = require('../controllers/biz.controller');
-
-  app.get('/biz', biz.getBiz);
-
-  app.post('/biz', biz.createBiz);
-
-  app.delete('/biz', biz.deleteBiz);
-};
