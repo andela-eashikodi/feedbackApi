ed450d558a5b9a2d3424123f19f8cdb02d33f8f2
andela-eashikodi <emeka.ashikodi@andela.co>
2015-06-17 17:42:38 +0100

    edit business route

--- app/controllers/business.controller.js
+++ app/controllers/business.controller.js
@@ -3,14 +3,21 @@ var mongoose = require('mongoose');
 
 require('../models/business.model');
 
+var formidable = require('formidable');
+var cloudinary = require('cloudinary');
+
 require('../models/user.model');
 
 var Business = mongoose.model('Business');
 
+cloudinary.config({
+	cloud_name: "shopal",
+	api_key: "732827613157146",
+	api_secret:"G96gDzcBkYcCthhhiQW4_kcNx6I"
+});
+
 exports.createBusiness = function(req, res) {
 	var business = new Business(req.body);
-	req.body.created_by = req.created_by;
-
 	business.save(req.body, function(err, business) {
 		if(err) {
 			return res.json(err);
@@ -26,6 +33,16 @@ exports.getBusiness = function(req, res) {
 		}
 		return res.json(business);
 	});
+};
+exports.getImage = function(req, res, next) {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err,fields,file){
+		req.body = fields;
+		cloudinary.uploader.upload(file.file.path, function(result) { 
+  		req.body.imageUrl = result.secure_url;
+  		next();
+		});
+	})
 };
 
 exports.deleteAll = function(req, res) {
@@ -47,7 +64,16 @@ exports.findOne = function(req, res) {
 };
 
 exports.findCategory = function(req, res) {
-	Business.find({Category: req.params.Category}, function(err, business) {
+	Business.find({category: req.params.category}, function(err, business) {
+		if(err) {
+			return res.json(err);
+		}
+		return res.json(business);
+	});
+};
+
+exports.findUserBusiness = function(req, res){
+	Business.find({created_by: req.params.createdby}).populate('created_by').exec(function(err, business) {
 		if(err) {
 			return res.json(err);
 		}
--- app/routes/business.route.js
+++ app/routes/business.route.js
@@ -6,15 +6,18 @@ module.exports = function(app) {
 	var business = require('../controllers/business.controller');
 
 	router.route('/business')
-		.post(business.createBusiness)
+		.post(business.getImage, business.createBusiness)
 		.get(business.getBusiness)
 		.delete(business.deleteAll);
 
 	router.route('/business/:BusinessName')
 		.get(business.findOne);
 
-	router.route('/business/categories/:Category')
+	router.route('/business/categories/:category')
 		.get(business.findCategory);
 
+	router.route('/business/user/:createdby')
+		.get(business.findUserBusiness);
+
 	app.use('/api', router);
-}
+};
